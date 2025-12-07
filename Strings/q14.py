fb2b0ebdb0dc4cd68887601051fb75e29339eab0
Harshit-077 <harshitsharma.0777@gmail.com>
2025-12-07 18:30:09 +0530

    Format Strings

---
+++ Strings/q14.py
@@ -0,0 +1,5 @@
+# Q14: Create an f-string:
+# also used .format() method
+name = "Harshit"
+print(f"Hello, {name}")
+print("Welcome, {}".format(name))
---
+++ Strings/q15.py
@@ -0,0 +1,3 @@
+# Q15: Display the price with 2 decimals:
+price = 50
+print(f"The cost is {price:.2f} rupees")
