d2cac504e0ab84cfb2e8b5a29a36ffb99ca0fa6a
Harshit Sharma <harshitsharma.0777@gmail.com>
2025-12-26 09:46:36 +0530

    copy list

---
+++ List/Q40.py
@@ -0,0 +1,4 @@
+#Q40 Make a copy of a list with the copy() method:
+li = [1,2,3,4,5]
+newli = li.copy()
+print(newli)
---
+++ List/Q42.py
@@ -0,0 +1,4 @@
+#Q42: Make a copy of a list with the : operator
+li = ["apple", "banana", "cherry"]
+mylist = li[:]
+print(mylist)
