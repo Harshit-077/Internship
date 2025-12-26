a8d465ed894aff9cb8a4d6cdc418ed584c192b05
Harshit Sharma <harshitsharma.0777@gmail.com>
2025-12-26 09:49:20 +0530

    Join List

---
+++ List/Q44.py
@@ -0,0 +1,6 @@
+#Q44: Append list2 into list1:
+l1 = [1,2,3,4,5]
+l2 = [10,9,8,7]
+for x in l2:
+    l1.append(x)
+print(l1)
---
+++ List/Q45.py
@@ -0,0 +1,5 @@
+#Q45: Use the extend() method to add list2 at the end of list1
+l1 = [1,2,3,4,5]
+l2 = [10,9,8,7]
+l1.extend(l2)
+print(l1)
