803a7cdec007be7d62a256bd73925da97a7bd86d
Harshit Sharma <harshitsharma.0777@gmail.com>
2025-12-28 15:32:01 +0530

    Access Set

---
+++ Sets/Q6.py
@@ -0,0 +1,4 @@
+# Loop through the set, and print the values:
+s1 = {1,2,3,4,5,"apple","banana"}
+for x in s1:
+    print(x)
---
+++ Sets/Q7.py
@@ -0,0 +1,4 @@
+# Check if "banana" is present in the set:
+s1 = {1,2,3,4,5,"apple","banana"}
+print("banana" in s1)
+print("banana" not in s1)
