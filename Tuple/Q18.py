9065e1b4875b9fc1b3343c8956138d92aec68369
Harshit Sharma <harshitsharma.0777@gmail.com>
2025-12-28 14:38:39 +0530

    Join tuples

---
+++ Tuple/Q18.py
@@ -0,0 +1,5 @@
+#Q18: Join two tuples:
+t1 = (1,2,3)
+t2 = (9,8,7)
+t3 = t1+t2
+print(t3)
---
+++ Tuple/Q19.py
@@ -0,0 +1,4 @@
+#Q19: Multiply the fruits tuple by 2:
+t1 = ("apple","banana","orange")
+t2 = t1*2
+print(t2)
