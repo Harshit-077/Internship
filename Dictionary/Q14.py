998cdee66ada0c71fd42d442a8a761dfd6f5aa8b
Harshit Sharma <harshitsharma.0777@gmail.com>
2025-12-30 13:27:50 +0530

    Add items

---
+++ Dictionary/Q14.py
@@ -0,0 +1,8 @@
+#Q14: Adding an item to the dictionary is done by using a new index key and assigning a value to it
+dct = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+dct["color"] = "white"
+print(dct)
---
+++ Dictionary/Q15.py
@@ -0,0 +1,8 @@
+# Add a color item to the dictionary by using the update() method:
+dct = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+dct.update({"color": "red"})
+print(dct)
