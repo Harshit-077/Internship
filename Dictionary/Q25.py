ef84c96755bd0c23d363e90c3fca2008718a4562
Harshit Sharma <harshitsharma.0777@gmail.com>
2025-12-30 13:44:46 +0530

    Copy Dict

---
+++ Dictionary/Q25.py
@@ -0,0 +1,8 @@
+# Make a copy of a dictionary with the copy() method:
+dct = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+newdict = dct.copy()
+print(newdict)
---
+++ Dictionary/Q26.py
@@ -0,0 +1,8 @@
+# Make a copy of a dictionary with the dict() function:
+dct = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+newdict = dict(dct)
+print(newdict)
