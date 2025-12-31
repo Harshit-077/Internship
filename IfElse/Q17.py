1d976ff7dac4354b85edfceed6b26e8f266c4fbf
Harshit Sharma <harshitsharma.0777@gmail.com>
2025-12-31 16:10:45 +0530

    Pass Statement

---
+++ IfElse/Q17.py
@@ -0,0 +1,7 @@
+# Placeholder for future implementation:
+age = 16
+
+if age < 18:
+  pass # TODO: Add underage logic
+else:
+  print("Access granted")
---
+++ IfElse/Q18.py
@@ -0,0 +1,5 @@
+# Using pass with functions:
+def func1():
+    pass
+
+# Function exists but doesn't do anything yet
