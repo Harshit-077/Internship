996ad2dfab0033a2ceeee43b56d3cfeef52d1d70
Harshit Sharma <harshitsharma.0777@gmail.com>
2026-01-04 14:45:55 +0530

    RegEx

---
+++ RegEx/Q2.py
@@ -0,0 +1,5 @@
+# Print a list of all matches:
+import re
+txt = "The rain in Spain"
+x = re.findall("ai",txt)
+print(x)
---
+++ RegEx/Q9.py
@@ -0,0 +1,6 @@
+# Do a search that will return a Match Object:
+import re
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x)
