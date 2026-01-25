e8bea3a545d9df17eff063165b75528faafd8b81
Harshit Sharma <harshitsharma.0777@gmail.com>
2026-01-25 16:12:32 +0530

    Random Intro

---
+++ Numpy/Random/Q2.py
@@ -0,0 +1,4 @@
+# Generate a random float from 0 to 1:
+from numpy import random
+x = random.rand()
+print(x)
---
+++ Numpy/Random/Q5.py
@@ -0,0 +1,5 @@
+# Return one of the values in an array:
+from numpy import random
+x = random.choice([3, 5, 7, 9])
+
+print(x)
