5288327ea62a18748de968afb536259a32b13fdb
Harshit Sharma <harshitsharma.0777@gmail.com>
2026-02-07 17:28:23 +0530

    Arithmetic

---
+++ Numpy/ufunc/Q11.py
@@ -0,0 +1,9 @@
+# Raise the values in arr1 to the power of values in arr2:
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([3, 5, 6, 8, 2, 33])
+
+ar = np.power(arr1, arr2)
+
+print(ar)
---
+++ Numpy/ufunc/Q15.py
@@ -0,0 +1,7 @@
+# Return the absolute values of the array:
+import numpy as np
+
+arr = np.array([-1, -2, 1, 2, 3, -4])
+ar = np.absolute(arr)
+
+print(ar)
