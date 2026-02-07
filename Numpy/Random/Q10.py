5eb1cd6ec2b6ffea56def49479c4caaae40795b9
Harshit Sharma <harshitsharma.0777@gmail.com>
2026-02-07 14:53:03 +0530

    Random

---
+++ Numpy/Random/Q10.py
@@ -0,0 +1,4 @@
+# Generate a random permutation of elements of following array:
+import numpy as np
+x = np.array([3, 5, 7, 9])
+print(np.random.permutation(x))
---
+++ Numpy/Random/Q9.py
@@ -0,0 +1,6 @@
+# Randomly shuffle elements of following array:
+import numpy as np
+x = np.array([3, 5, 7, 9])
+np.random.shuffle(x)
+print(x)
+
