0c5588de9f53945eef814c00b0885e92bdb7ad5d
Harshit Sharma <harshitsharma.0777@gmail.com>
2026-02-07 14:55:23 +0530

    Normal Distribution

---
+++ Numpy/Distribution/Q1.py
@@ -0,0 +1,4 @@
+# Generate a random normal distribution of size 2x3:
+import numpy as np
+x = np.random.normal(size=(2,3))
+print(x)
---
+++ Numpy/Distribution/Q2.py
@@ -0,0 +1,4 @@
+# Generate a random normal distribution of size 2x3 with mean at 1 and standard deviation of 2:
+import numpy as np
+x = np.random.normal(1,2,size=(2,3))
+print(x)
