4f274845755ae49010c7dfc7c2c1a1248e63a80a
Harshit Sharma <harshitsharma.0777@gmail.com>
2026-02-09 20:08:38 +0530

    Decimals

---
+++ Numpy/ufunc/Q17.py
@@ -0,0 +1,5 @@
+# Round off 3.1666 to 2 decimal places:
+import numpy as np
+arr = np.around(3.1666, 2)
+
+print(arr)
---
+++ Numpy/ufunc/Q18.py
@@ -0,0 +1,5 @@
+# Floor the elements of following array:
+import numpy as np
+arr = np.floor([-3.1666, 3.6667])
+
+print(arr)
