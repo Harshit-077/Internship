a54e376a49d685d80828ee6191c0723d0ef403d9
Harshit Sharma <harshitsharma.0777@gmail.com>
2026-02-09 21:34:14 +0530

    Differences

---
+++ Numpy/ufunc/Q30.py
@@ -0,0 +1,6 @@
+# Compute discrete difference of the following array:
+import numpy as np
+arr = np.array([10, 15, 25, 5])
+ar = np.diff(arr)
+
+print(ar)
---
+++ Numpy/ufunc/Q31.py
@@ -0,0 +1,6 @@
+# Compute discrete difference of the following array twice:
+import numpy as np
+arr = np.array([10, 15, 25, 5])
+ar = np.diff(arr, n=2)
+
+print(ar)
